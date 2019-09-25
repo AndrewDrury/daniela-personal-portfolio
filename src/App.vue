940257c37d9743d777522e12a5f1cf691a2520a8
AndrewDrury <drurya99@gmail.com>
2019-09-25 17:01:28 -0400

    running lint

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,36 @@
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
         <b-navbar-brand to="/">
           <img
-            srcset="@/assets/homepage/logo.png, @/assets/homepage/logo@2x.png 2x, @/assets/homepage/logo@3x.png 3x"
+            srcset="
+              @/assets/homepage/logo.png,
+              @/assets/homepage/logo@2x.png 2x,
+              @/assets/homepage/logo@3x.png 3x
+            "
           />
         </b-navbar-brand>
         <b-collapse is-nav id="nav_collapse">
           <!-- Right aligned nav items, collapsed on mobile -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item class="tab" href="/daniela-personal-portfolio/#Projects" right>projects</b-nav-item>
-            <b-nav-item class="tab" href="./DanielaOrnelasResume.pdf" target="_blank" right>resume</b-nav-item>
+            <b-nav-item
+              class="tab"
+              href="/daniela-personal-portfolio/#Projects"
+              right
+              >projects</b-nav-item
+            >
+            <b-nav-item
+              class="tab"
+              href="./DanielaOrnelasResume.pdf"
+              target="_blank"
+              right
+              >resume</b-nav-item
+            >
             <b-nav-item class="tab" to="/about" right>about</b-nav-item>
-            <b-nav-item class="tab" href="mailto:ddanielaornelas@gmail.com" right>let's chat!</b-nav-item>
+            <b-nav-item
+              class="tab"
+              href="mailto:ddanielaornelas@gmail.com"
+              right
+              >let's chat!</b-nav-item
+            >
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
@@ -50,10 +70,17 @@
           <b-col class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
             <ul class="list-unstyled list-inline social text-center">
               <li class="list-inline-item">
-                <a href="https://medium.com/@danielaornelas_40338" target="_blank">
+                <a
+                  href="https://medium.com/@danielaornelas_40338"
+                  target="_blank"
+                >
                   <!-- <i class="fa fa-medium"></i> -->
                   <img
-                    srcset="@/assets/homepage/medium/Shape.png, @/assets/homepage/medium/Shape@2x.png 2x, @/assets/homepage/medium/Shape@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/medium/Shape.png,
+                      @/assets/homepage/medium/Shape@2x.png 2x,
+                      @/assets/homepage/medium/Shape@3x.png 3x
+                    "
                     style="width:1.25em"
                   />
                 </a>
@@ -62,16 +89,27 @@
                 <a href="mailto:ddanielaornelas@gmail.com" target="_blank">
                   <!-- <i class="fa fa-envelope"></i> -->
                   <img
-                    srcset="@/assets/homepage/envelope.png, @/assets/homepage/envelope@2x.png 2x, @/assets/homepage/envelope@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/envelope.png,
+                      @/assets/homepage/envelope@2x.png 2x,
+                      @/assets/homepage/envelope@3x.png 3x
+                    "
                     style="width:1.25em"
                   />
                 </a>
               </li>
               <li class="list-inline-item">
-                <a href="https://ca.linkedin.com/in/danielaornelas334" target="_blank">
+                <a
+                  href="https://ca.linkedin.com/in/danielaornelas334"
+                  target="_blank"
+                >
                   <!-- <i class="fa fa-linkedin"></i> -->
                   <img
-                    srcset="@/assets/homepage/linkedin-logo.png, @/assets/homepage/linkedin-logo@2x.png 2x, @/assets/homepage/linkedin-logo@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/linkedin-logo.png,
+                      @/assets/homepage/linkedin-logo@2x.png 2x,
+                      @/assets/homepage/linkedin-logo@3x.png 3x
+                    "
                     style="width:1em"
                   />
                 </a>
@@ -80,7 +118,9 @@
           </b-col>
         </b-row>
         <b-row>
-          <b-col class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+          <b-col
+            class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white"
+          >
             <p class="h6">&copy; Daniela Ornelas</p>
           </b-col>
         </b-row>

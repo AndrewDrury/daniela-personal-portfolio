7f17b89a4795d8b83c224e26d98feb5bf37e8afd
Andrew Drury <drurya99@gmail.com>
2019-10-10 22:02:21 -0400

    Attempt to fix 2

---
+++ src/views/Empty.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="empty">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Empty extends Vue {}
+</script>

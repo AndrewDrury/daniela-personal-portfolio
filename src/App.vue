f749f349a788a5ccb261de0e022b90e1ca255fd4
Andrew Drury <drurya99@gmail.com>
2019-10-10 21:42:46 -0400

    new pics

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,13 @@
             href="https://www.dropbox.com/sh/vcb40o2liwopshn/AABSz1u5-BVe4v6mtpg0E1JEa?dl=0&preview=Resume.pdf"
             target="_blank"
             right
-          >resume</b-nav-item>
+            >resume</b-nav-item
+          >
           <b-nav-item class="tab" to="/about" right>about</b-nav-item>
           <!-- <b-nav-item class="tab" to="/ePaid" right>ePaid</b-nav-item> -->
-          <b-nav-item class="tab" href="mailto:ddanielaornelas@gmail.com" right>let's chat!</b-nav-item>
+          <b-nav-item class="tab" href="mailto:ddanielaornelas@gmail.com" right
+            >let's chat!</b-nav-item
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -41,11 +44,15 @@
               <b-col md="11" class="text-left">
                 <h4 class="introduction" style="margin-bottom: 60px">
                   I’m a curious
-                  <b class="bolded" style="font-size: 30px">UX designer.</b> I ask
-                  all the questions, so that the user doesn’t have to. Living and
-                  learning in Toronto, Canada.
+                  <b class="bolded" style="font-size: 30px">UX designer.</b> I
+                  ask all the questions, so that the user doesn’t have to.
+                  Living and learning in Toronto, Canada.
                   <img
-                    srcset="@/assets/homepage/maple-leaf/Shape.png, @/assets/homepage/maple-leaf/Shape@2x.png 2x, @/assets/homepage/maple-leaf/Shape@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/maple-leaf/Shape.png,
+                      @/assets/homepage/maple-leaf/Shape@2x.png 2x,
+                      @/assets/homepage/maple-leaf/Shape@3x.png 3x
+                    "
                   />
                 </h4>
               </b-col>
@@ -57,14 +64,21 @@
                     My Projects
                     <img
                       class="arrow-down pb-2"
-                      srcset="@/assets/homepage/next.png, @/assets/homepage/next@2x.png 2x, @/assets/homepage/next@3x.png 3x"
+                      srcset="
+                        @/assets/homepage/next.png,
+                        @/assets/homepage/next@2x.png 2x,
+                        @/assets/homepage/next@3x.png 3x
+                      "
                     />
                   </h2>
                 </a>
                 <b-row class="projectRow">
                   <b-col lg="5" align="left">
                     <a href="./ePaid">
-                      <img class="screenshot-epaid" src="@/assets/homepage/ePaidMain.png" />
+                      <img
+                        class="screenshot-epaid"
+                        src="@/assets/homepage/ePaidMain.png"
+                      />
                     </a>
                   </b-col>
                   <b-col lg="7" align="left">
@@ -101,16 +115,26 @@
                   <b-col class="desktop" lg="7" align="left">
                     <h3 class="project-title">Fiti</h3>
                     <h4 class="category">Passion Project</h4>
-                    <h5 class="tag-line">A fitness app for people with paraplegia.</h5>
+                    <h5 class="tag-line">
+                      A fitness app for people with paraplegia.
+                    </h5>
                     <p class="description">
                       Fiti provides accessible, affordable, and interesting
                       workouts that are easy to incorperate in all lifestyles.
                     </p>
                     <!-- <a class="read-more" href="./Fiti"> -->
-                    <a class="read-more" href="../Fiti-info.pdf" target="_blank">
+                    <a
+                      class="read-more"
+                      href="../Fiti-info.pdf"
+                      target="_blank"
+                    >
                       Read More
                       <img
-                        srcset="@/assets/homepage/next.png, @/assets/homepage/next@2x.png 2x, @/assets/homepage/next@3x.png 3x"
+                        srcset="
+                          @/assets/homepage/next.png,
+                          @/assets/homepage/next@2x.png 2x,
+                          @/assets/homepage/next@3x.png 3x
+                        "
                       />
                     </a>
                     <h6 class="skill-title">Skills</h6>
@@ -128,23 +152,37 @@
                     <a href="../Fiti-info.pdf" target="_blank">
                       <img
                         class="screenshot"
-                        srcset="@/assets/homepage/dark.png, @/assets/homepage/dark@2x.png 2x, @/assets/homepage/dark@3x.png 3x"
+                        srcset="
+                          @/assets/homepage/dark.png,
+                          @/assets/homepage/dark@2x.png 2x,
+                          @/assets/homepage/dark@3x.png 3x
+                        "
                       />
                     </a>
                   </b-col>
                   <b-col class="mobile" align="left">
                     <h3 class="project-title">Fiti</h3>
                     <h4 class="category">Passion Project</h4>
-                    <h5 class="tag-line">A fitness app for people with paraplegia.</h5>
+                    <h5 class="tag-line">
+                      A fitness app for people with paraplegia.
+                    </h5>
                     <p class="description">
                       Fiti provides accessible, affordable, and interesting
                       workouts that are easy to incorperate in all lifestyles.
                     </p>
                     <!-- <a class="read-more" href="./Fiti"> -->
-                    <a class="read-more" href="../Fiti-info.pdf" target="_blank">
+                    <a
+                      class="read-more"
+                      href="../Fiti-info.pdf"
+                      target="_blank"
+                    >
                       Read More
                       <img
-                        srcset="@/assets/homepage/next.png, @/assets/homepage/next@2x.png 2x, @/assets/homepage/next@3x.png 3x"
+                        srcset="
+                          @/assets/homepage/next.png,
+                          @/assets/homepage/next@2x.png 2x,
+                          @/assets/homepage/next@3x.png 3x
+                        "
                       />
                     </a>
                     <h6 class="skill-title">Skills</h6>
@@ -165,23 +203,37 @@
                     <a href="../HealthLine-info.pdf" target="_blank">
                       <img
                         class="screenshot"
-                        srcset="@/assets/HealthLine-info/Group10.png, @/assets/HealthLine-info/Group10@2x.png 2x, @/assets/HealthLine-info/Group10@3x.png 3x"
+                        srcset="
+                          @/assets/HealthLine-info/Group10.png,
+                          @/assets/HealthLine-info/Group10@2x.png 2x,
+                          @/assets/HealthLine-info/Group10@3x.png 3x
+                        "
                       />
                     </a>
                   </b-col>
                   <b-col lg="7" align="left">
                     <h3 class="project-title">HealthLine</h3>
                     <h4 class="category">RU Hacks</h4>
-                    <h5 class="tag-line">Receive a diagnosis of your symptoms.</h5>
+                    <h5 class="tag-line">
+                      Receive a diagnosis of your symptoms.
+                    </h5>
                     <p class="description">
                       A diagnosis is made through computer vision and machine
                       learning, and SMS.
                     </p>
                     <!-- <a class="read-more" href="./HealthLine">-->
-                    <a class="read-more" href="../HealthLine-info.pdf" target="_blank">
+                    <a
+                      class="read-more"
+                      href="../HealthLine-info.pdf"
+                      target="_blank"
+                    >
                       Read More
                       <img
-                        srcset="@/assets/homepage/next.png, @/assets/homepage/next@2x.png 2x, @/assets/homepage/next@3x.png 3x"
+                        srcset="
+                          @/assets/homepage/next.png,
+                          @/assets/homepage/next@2x.png 2x,
+                          @/assets/homepage/next@3x.png 3x
+                        "
                       />
                     </a>
                     <h6 class="skill-title">Skills</h6>
@@ -224,10 +276,17 @@
           <b-col class="col-xs-12 col-sm-12 col-md-12">
             <ul class="list-unstyled list-inline social text-center">
               <li class="list-inline-item">
-                <a href="https://medium.com/@danielaornelas_40338" target="_blank">
+                <a
+                  href="https://medium.com/@danielaornelas_40338"
+                  target="_blank"
+                >
                   <!-- <i class="fa fa-medium"></i> -->
                   <img
-                    srcset="@/assets/homepage/medium/Shape.png, @/assets/homepage/medium/Shape@2x.png 2x, @/assets/homepage/medium/Shape@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/medium/Shape.png,
+                      @/assets/homepage/medium/Shape@2x.png 2x,
+                      @/assets/homepage/medium/Shape@3x.png 3x
+                    "
                     style="width:1.25em"
                   />
                 </a>
@@ -236,16 +295,27 @@
                 <a href="mailto:ddanielaornelas@gmail.com" target="_blank">
                   <!-- <i class="fa fa-envelope"></i> -->
                   <img
-                    srcset="@/assets/homepage/envelope.png, @/assets/homepage/envelope@2x.png 2x, @/assets/homepage/envelope@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/envelope.png,
+                      @/assets/homepage/envelope@2x.png 2x,
+                      @/assets/homepage/envelope@3x.png 3x
+                    "
                     style="width:1.25em"
                   />
                 </a>
               </li>
               <li class="list-inline-item">
-                <a href="https://ca.linkedin.com/in/danielaornelas334" target="_blank">
+                <a
+                  href="https://ca.linkedin.com/in/danielaornelas334"
+                  target="_blank"
+                >
                   <!-- <i class="fa fa-linkedin"></i> -->
                   <img
-                    srcset="@/assets/homepage/linkedin-logo.png, @/assets/homepage/linkedin-logo@2x.png 2x, @/assets/homepage/linkedin-logo@3x.png 3x"
+                    srcset="
+                      @/assets/homepage/linkedin-logo.png,
+                      @/assets/homepage/linkedin-logo@2x.png 2x,
+                      @/assets/homepage/linkedin-logo@3x.png 3x
+                    "
                     style="width:1em"
                   />
                 </a>

990ee0260934a4f33720e5104829a217f9b47b95
Andrew Drury <andrew.drury@stratus360.com>
2019-10-15 11:28:51 -0400

    using hash mode

--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,7 @@ import HealthLine from "./views/projects/HealthLine.vue";
 Vue.use(Router);
 
 export default new Router({
-  // mode: "history",
+  // mode: "history", //If commented out, use hash mode
   base: process.env.BASE_URL,
   routes: [
     {
